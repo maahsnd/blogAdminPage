ef798cc914a48120ac480c1842f31ba2c2a68719
mrwindupbirdesq <nmaahs11@gmail.com>
2023-10-09 15:21:38 -0700

    Add route

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { UserProvider } from './components/UserContext';
-import ProtectedRoute from './components/ProtectedRoute';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
+import AllPosts from './components/AllPosts';
+import Post from './components/Post';
 
 function App() {
   return (
@@ -11,7 +12,10 @@ function App() {
       <UserProvider>
         <Routes>
           <Route path="/log-in" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/posts/:id" element={<Post />} />
+          <Route path="/posts" element={<AllPosts />} />
+          {/*           <Route path="/new-post" element={<NewPost />} /> */}
+          <Route path="/" element={<Dashboard />} />
         </Routes>
       </UserProvider>
     </Router>
