7bad8f5ff0e5c65fc5137cc82af0cf5f6d2b8576
mrwindupbirdesq <nmaahs11@gmail.com>
2023-10-11 14:20:07 -0700

    Implement log out

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -5,12 +5,31 @@ import { useUserContext } from './UserContext';
 
 function Dashboard() {
   const { user, setUser } = useUserContext();
+  const logOut = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/users/log-out', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      if (response.ok) {
+        setUser(null);
+        return;
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
   if (user && user.authenticated) {
     return (
       <div>
-        <h2>Welcome to the Dashboard</h2>
+        <h2>Welcome to the Dashboard, {user.user_name}</h2>
         <Link to="/posts">View All Posts</Link>
+        <hr />
         <Link to="/posts/new">Create New Post</Link>
+        <hr />
+        <button onClick={logOut}>Log Out</button>
       </div>
     );
   } else {
